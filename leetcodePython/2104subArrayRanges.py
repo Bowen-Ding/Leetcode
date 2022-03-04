a122d8ea4b03d1db95af49925b0644c244303dae
Bowen-Ding <1062682058@qq.com>
2022-03-04 20:09:56 +0800

    2104 子数组范围和

---
+++ leetcodePython/2104subArrayRanges.py
@@ -0,0 +1,90 @@
+'''
+给你一个整数数组 nums 。nums 中，子数组的 范围 是子数组中最大元素和最小元素的差值。
+
+返回 nums 中 所有 子数组范围的 和 。
+
+子数组是数组中一个连续 非空 的元素序列。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：4
+解释：nums 的 6 个子数组如下所示：
+[1]，范围 = 最大 - 最小 = 1 - 1 = 0 
+[2]，范围 = 2 - 2 = 0
+[3]，范围 = 3 - 3 = 0
+[1,2]，范围 = 2 - 1 = 1
+[2,3]，范围 = 3 - 2 = 1
+[1,2,3]，范围 = 3 - 1 = 2
+所有范围的和是 0 + 0 + 0 + 1 + 1 + 2 = 4
+示例 2：
+
+输入：nums = [1,3,3]
+输出：4
+解释：nums 的 6 个子数组如下所示：
+[1]，范围 = 最大 - 最小 = 1 - 1 = 0
+[3]，范围 = 3 - 3 = 0
+[3]，范围 = 3 - 3 = 0
+[1,3]，范围 = 3 - 1 = 2
+[3,3]，范围 = 3 - 3 = 0
+[1,3,3]，范围 = 3 - 1 = 2
+所有范围的和是 0 + 0 + 0 + 2 + 0 + 2 = 4
+示例 3：
+
+输入：nums = [4,-2,-3,4,1]
+输出：59
+解释：nums 中所有子数组范围的和是 59
+ 
+
+提示：
+
+1 <= nums.length <= 1000
+-109 <= nums[i] <= 109
+'''
+
+from numpy import append
+
+
+class Solution(object):
+    def subArrayRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        minLeft, maxLeft = [0] * n, [0] * n
+        minStack, maxStack = [], []
+        for i,num in enumerate(nums):
+            while minStack and nums[minStack[-1]] > num:
+                minStack.pop()
+            minLeft[i] = minStack[-1] if minStack else -1
+            minStack.append(i)
+
+            while maxStack and nums[maxStack[-1]] <= num:
+                maxStack.pop()
+            maxLeft[i] = maxStack[-1] if maxStack else -1
+            maxStack.append(i)
+
+        minRight, maxRight = [0] * n, [0] * n
+        minStack, maxStack = [], []
+        for i in range(n-1, -1, -1):
+            num = nums[i]
+            while minStack and nums[minStack[-1]] >= num:
+                minStack.pop()
+            minRight[i] = minStack[-1] if minStack else n
+            minStack.append(i)
+
+            while maxStack and nums[maxStack[-1]] < num:
+                maxStack.pop()
+            maxRight[i] = maxStack[-1] if maxStack else n
+            maxStack.append(i)
+
+        sumMax, sumMin = 0, 0
+        for i, num in enumerate(nums):
+            sumMax += (maxRight[i] - i) * (i - maxLeft[i]) * num
+            sumMin += (minRight[i] - i) * (i - minLeft[i]) * num
+
+        return sumMax - sumMin
+        
